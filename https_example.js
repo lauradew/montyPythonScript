e33d63ad1db94fc7f0f30f765b284930f32f0bac
Laura De Witt <lauramdewitt@gmail.com>
2018-01-22 04:09:46 +0000

    showing first chunk of script that is received from server

--- https_example.js
+++ https_example.js
@@ -19,15 +19,27 @@ console.log("I'm about to make the request!");
 //   console.log('Response: ', response);
 // }
 
+var chunkBits = [];
 //calls for outputting data
 var callback = function(response) {
   console.log("I've made the request!");
   console.log('In response handler callback!');
 
-  response to show all chunks
+  // response to show all chunks
+  // response.on('data', function(chunk) {
+  //   console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+  //   console.log(chunk.toString());
+  // });
+
+  //resonse to showing contents of first chunk received
   response.on('data', function(chunk) {
+    chunkBits.push(chunk.toString());
     console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
-    console.log(chunk.toString());
+    //checking lengths of chunks
+    // console.log(chunkBits.length);
+    if (chunkBits.length === 1) {
+      console.log(chunkBits[0]);
+    }
   });
 }
 
