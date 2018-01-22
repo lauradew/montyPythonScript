c2350a7de2beaf55fedfdf1e31dbd6942363d9fe
Laura De Witt <lauramdewitt@gmail.com>
2018-01-22 03:44:20 +0000

    submitting chunks for montypython

--- https_example.js
+++ https_example.js
@@ -3,8 +3,8 @@ var https = require('https');
 // console.log('I did it!');
 
 var options = {
-  host: 'www.example.org',
-  path: '/'
+  host: 'stream-large-file.herokuapp.com',
+  path: '/give-me-stuff-now'
 };
 
 
@@ -24,6 +24,7 @@ var callback = function(response) {
   console.log("I've made the request!");
   console.log('In response handler callback!');
 
+  response to show all chunks
   response.on('data', function(chunk) {
     console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
     console.log(chunk.toString());
